09eaf75527f05655d94f5c21e2812b22a962b41b
celso <celso.cunha.rodrigues@gmail.com>
2021-05-16 09:58:49 +0000

    testing a terraform provider to install the ingress operator

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -42,4 +42,20 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
 
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+}
